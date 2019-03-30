c9b41721931fd356728444e569e3514b90442ff3
Sashok256 <gribmail@gmail.com>
2019-03-30 21:53:09 +0200

    fixed tabs

--- client/src/app/app_models/user.ts
+++ client/src/app/app_models/user.ts
@@ -1,24 +1,24 @@
 export class User {
-    id: number;
-    firstName: string;
-    lastName: string;
-    position: string;
-    manager: object;
-    teamlead: object;
-    department: object;
-    email: string;
-    phone: string;
-    contacts: object;
-    photoURL: string;
-    hrID: number;
-    date: object;
+  id: number;
+  firstName: string;
+  lastName: string;
+  position: string;
+  manager: object;
+  teamlead: object;
+  department: object;
+  email: string;
+  phone: string;
+  contacts: object;
+  photoURL: string;
+  hrID: number;
+  date: object;
   
-    constructor(values: Object = {}) {
-      for (const key in values) {
-        this[key] = values[key];
-      }
+  constructor(values: Object = {}) {
+    for (const key in values) {
+      this[key] = values[key];
     }
   }
+}
   
   
   
