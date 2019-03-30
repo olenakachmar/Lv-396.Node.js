1c27715c19ab4a9598b7cf9ec57c855625ac681d
ira.che <ira.chernomorets@gmail.com>
2019-03-30 17:23:26 +0200

    refactor filter pipe function

--- client/src/app/page/filter-tasks-by.pipe.spec.ts
+++ client/src/app/page/filter-tasks-by.pipe.spec.ts
@@ -8,63 +8,53 @@ describe('FilterTasksByPipe', () => {
   const tasks: Task[] = [
     {
       id: 0,
-      name: 'Upcoming task name',
-      excerpt: 'This content is straight in the template.',
+      name: '',
+      excerpt: '',
       status: { name: 'LOW', value: 2 },
       type: { name: 'issue', value: 1 },
       date: '22/03/2019',
-      author: 'Alex Somename',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
-        'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ' +
-        'ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis',
+      author: '',
+      content: '',
     },
     {
       id: 1,
-      name: 'Upcoming task name2',
-      excerpt: 'This content is straight in the template2.',
+      name: '',
+      excerpt: '',
       status: { name: 'HIGHT', value: 0 },
       type: { name: 'issue', value: 1 },
       date: '23/03/2019',
-      author: 'Alex3 Somename',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
-        'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ' +
-        'ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis',
+      author: '',
+      content: '',
     },
     {
       id: 2,
-      name: 'Upcoming task name3',
-      excerpt: 'This content is straight in the template3.',
+      name: '',
+      excerpt: '',
       status: { name: 'LOW', value: 2 },
       type: { name: 'task', value: 0 },
       date: '24/03/2019',
-      author: 'Alex2 Somename',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ' +
-        'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' +
-        'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis',
+      author: '',
+      content: '',
     },
     {
       id: 3,
-      name: 'Upcoming task name4',
-      excerpt: 'This content is straight in the template4.',
+      name: '',
+      excerpt: '',
       status: { name: 'NORMAL', value: 1 },
       type: { name: 'task', value: 0 },
       date: '25/03/2019',
-      author: 'Alex1 Somename',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ' +
-        'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' +
-        'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis',
+      author: '',
+      content: '',
     },
     {
       id: 4,
-      name: 'Upcoming task name5',
-      excerpt: 'This content is straight in the template5.',
+      name: '',
+      excerpt: '',
       status: { name: 'LOW', value: 2 },
       type: { name: 'task', value: 0 },
       date: '26/03/2019',
-      author: 'Alex2 Somename',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ' +
-        'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' +
-        'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis',
+      author: '',
+      content: '',
     }
   ];
 
@@ -169,27 +159,23 @@ describe('FilterTasksByPipe', () => {
     const expectedResult: Task[] = [
       {
         id: 2,
-        name: 'Upcoming task name3',
-        excerpt: 'This content is straight in the template3.',
+        name: '',
+        excerpt: '',
         status: { name: 'LOW', value: 2 },
         type: { name: 'task', value: 0 },
         date: '24/03/2019',
-        author: 'Alex2 Somename',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ' +
-          'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' +
-          'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis',
+        author: '',
+        content: '',
       },
       {
         id: 4,
-        name: 'Upcoming task name5',
-        excerpt: 'This content is straight in the template5.',
+        name: '',
+        excerpt: '',
         status: { name: 'LOW', value: 2 },
         type: { name: 'task', value: 0 },
         date: '26/03/2019',
-        author: 'Alex2 Somename',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ' +
-          'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' +
-          'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis',
+        author: '',
+        content: '',
       }
     ];
     expect(pipe.transform(tasks, filters)).toEqual(expectedResult);
@@ -225,15 +211,13 @@ describe('FilterTasksByPipe', () => {
     const expectedResult: Task[] = [
       {
         id: 0,
-        name: 'Upcoming task name',
-        excerpt: 'This content is straight in the template.',
+        name: '',
+        excerpt: '',
         status: { name: 'LOW', value: 2 },
         type: { name: 'issue', value: 1 },
         date: '22/03/2019',
-        author: 'Alex Somename',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ' +
-          'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ' +
-          'ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis',
+        author: '',
+        content: '',
       }
     ];
     expect(pipe.transform(tasks, filters)).toEqual(expectedResult);
@@ -269,15 +253,13 @@ describe('FilterTasksByPipe', () => {
     const expectedResult: Task[] = [
       {
         id: 3,
-        name: 'Upcoming task name4',
-        excerpt: 'This content is straight in the template4.',
+        name: '',
+        excerpt: '',
         status: { name: 'NORMAL', value: 1 },
         type: { name: 'task', value: 0 },
         date: '25/03/2019',
-        author: 'Alex1 Somename',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ' +
-          'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ' +
-          'exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis',
+        author: '',
+        content: '',
       }
     ];
     expect(pipe.transform(tasks, filters)).toEqual(expectedResult);
@@ -341,5 +323,65 @@ describe('FilterTasksByPipe', () => {
       }
     ];
     expect(pipe.transform(tasks, filters)).toEqual(tasks.filter(item => item.id === 3));
+  });
+
+  it('task has by status LOW and type TASK', () => {
+    const pipe = new FilterTasksByPipe();
+    filters = [
+      {
+        id: 0,
+        name: 'type',
+        isCalendar: false,
+        defaultValue: 0,
+        options: [
+          { name: 'Show all tasks', value: -1 },
+          { name: 'Show delegates tasks only', value: 0 },
+          { name: 'Show issues only', value: 1 },
+        ],
+      },
+      {
+        id: 1,
+        name: 'status',
+        isCalendar: false,
+        defaultValue: 2,
+        options: [
+          { name: 'Filter by Status', value: -1 },
+          { name: 'High', value: 0 },
+          { name: 'Normal', value: 1 },
+          { name: 'Low', value: 2 },
+        ],
+      }
+    ];
+    expect(pipe.isTaskMatchesFilters(tasks[0], filters)).toEqual(false);
+  });
+
+  it('task has by status LOW and type ISSUE', () => {
+    const pipe = new FilterTasksByPipe();
+    filters = [
+      {
+        id: 0,
+        name: 'type',
+        isCalendar: false,
+        defaultValue: 1,
+        options: [
+          { name: 'Show all tasks', value: -1 },
+          { name: 'Show delegates tasks only', value: 0 },
+          { name: 'Show issues only', value: 1 },
+        ],
+      },
+      {
+        id: 1,
+        name: 'status',
+        isCalendar: false,
+        defaultValue: 2,
+        options: [
+          { name: 'Filter by Status', value: -1 },
+          { name: 'High', value: 0 },
+          { name: 'Normal', value: 1 },
+          { name: 'Low', value: 2 },
+        ],
+      }
+    ];
+    expect(pipe.isTaskMatchesFilters(tasks[0], filters)).toEqual(true);
   });
 });
--- client/src/app/page/filter-tasks-by.pipe.ts
+++ client/src/app/page/filter-tasks-by.pipe.ts
@@ -1,13 +1,32 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Filter } from './common/filter';
+import { Task } from './common/task';
 
 @Pipe({
   name: 'filterTasksBy'
 })
 export class FilterTasksByPipe implements PipeTransform {
 
-  transform(value: any, filters?: any): any {
-    return (!filters || filters.every(f => f.defaultValue === -1)) ? value : value.filter(
-        item => filters.every( f => f.defaultValue === -1 || f.defaultValue === item[f.name].value) );
+  transform(tasks: Task[], filters?: Filter[]): Task[] {
+    return (!filters || filters.every(f => f.defaultValue === -1)) ? tasks : tasks.filter(
+      task => this.isTaskMatchesFilters(task, filters));
+  }
+
+  isTaskMatchesFilters = (task: any, filters: Filter[]) => {
+    return filters.every( filter => {
+      let result = false;
+      if (filter.defaultValue === -1) {
+        result = true;
+      } else {
+        const meta = filter.name;
+        if (meta === 'date') {
+          result = (filter.defaultValue === task[meta]) ? true : false;
+        } else {
+          result = (filter.defaultValue === task[meta].value) ? true : false;
+        }
+      }
+      return result;
+    });
   }
 
 }
