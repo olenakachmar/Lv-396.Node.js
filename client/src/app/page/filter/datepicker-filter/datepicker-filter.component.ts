8a4da3dbfadabd80b5b515585aeb2170aa0349f7
ira.che <ira.chernomorets@gmail.com>
2019-04-10 18:24:30 +0300

    fix bugs

--- client/src/app/page/filter/datepicker-filter/datepicker-filter.component.ts
+++ client/src/app/page/filter/datepicker-filter/datepicker-filter.component.ts
@@ -22,6 +22,8 @@ export class DatepickerFilterComponent implements OnInit {
   choiseReadyFlag: number;
   cancelLinkText: string;
 
+  private _defaultTitle: string;
+
   constructor(private readonly dropDownService: DropDownService) { }
 
   ngOnInit(): void {
@@ -32,6 +34,12 @@ export class DatepickerFilterComponent implements OnInit {
     this.isDropup = false;
     this.getDropDownPositionClassNames();
     this.choiseReadyFlag = 0;
+  }
+
+  get defaultTitle(): string {
+    this._defaultTitle = this.filterItem.options.filter((item: FilterOptions) => item.value === -1)[0].name;
+
+    return this._defaultTitle;
   }
 
   private readonly getTitle = (titleObj: any) => {
@@ -123,10 +131,7 @@ export class DatepickerFilterComponent implements OnInit {
   };
 
   private readonly updateTitle = (takeDateToTitle: boolean = false): void => {
-    this.title = takeDateToTitle ? this.filterResult : this.getDefaultTitle();
+    this.title = takeDateToTitle ? this.filterResult : this.defaultTitle;
   };
-
-  private readonly getDefaultTitle = (): string =>
-    this.filterItem.options.filter((item: FilterOptions) => item.value === -1)[0].name;
 
 }
