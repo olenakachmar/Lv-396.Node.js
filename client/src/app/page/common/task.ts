6813124157405ba0b8b7567fd272c47df6c93044
Olena Kachmar <olena.kachmar9@gmail.com>
2019-04-11 12:57:42 +0300

    small fixes

--- client/src/app/page/common/task.ts
+++ client/src/app/page/common/task.ts
@@ -1,27 +1,23 @@
 export class TaskStatus {
-  constructor(
-    public name?: string,
-    public value?: number
-  ) {}
+  constructor(public name?: string,
+              public value?: number) {
+  }
 }
 
 export class TaskType {
-  constructor(
-    public name?: string,
-    public value?: number
-  ) {}
+  constructor(public name?: string,
+              public value?: number) {
+  }
 }
 
 export class Task {
-  constructor(
-    public id?: number,
-    public name?: string,
-    public excerpt?: string,
-    public status: TaskStatus = new TaskStatus(),
-    public type: TaskType = new TaskType(),
-    public date?: string,
-    public author?: string,
-    public content?: string
-  ) {
+  constructor(public id?: number,
+              public name?: string,
+              public excerpt?: string,
+              public status: TaskStatus = new TaskStatus(),
+              public type: TaskType = new TaskType(),
+              public date?: string,
+              public author?: string,
+              public content?: string) {
   }
 }
