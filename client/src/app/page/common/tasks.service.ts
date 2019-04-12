1dc1c8ba11699b1dc826344e29e8085aa15bb394
ira.che <ira.chernomorets@gmail.com>
2019-04-12 16:07:08 +0300

    quick fix

--- client/src/app/page/common/tasks.service.ts
+++ client/src/app/page/common/tasks.service.ts
@@ -4,25 +4,17 @@ import { Task } from './task';
 import { HttpClient } from '@angular/common/http';
 import { httpOptions } from '../../app_services/user.service';
 import { api } from '../../../environments/environment';
-// import 'rxjs/add/operator/map';
-// import 'rxjs/add/operator/catch';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class TasksService {
-  // statusOptions: { Status: {}, Type: {} };
-
   constructor(private readonly http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(`${api}/issues/all`, httpOptions);
   }
-
-  // handleError(err: Response | any): any {
-  //   return throwError(err);
-  // }
 
   update(task: any): any {
     return this.http.put(`${api}/issues`, task, httpOptions);
