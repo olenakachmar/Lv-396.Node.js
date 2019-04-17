5b937ea999a679c2bb1782dc676207cf11f1ec46
GrishaShats <grishashats0@gmail.com>
2019-04-17 19:32:48 +0300

    For pull

--- client/src/app/page/upcoming-dates/list-dates/list-dates.component.ts
+++ client/src/app/page/upcoming-dates/list-dates/list-dates.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../common/services/user.service';
+import { User } from '../../../common/models/user';
+import { DatesItem } from '../../common/dates-item';
 
 @Component({
   selector: 'app-list-dates',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-dates.component.scss']
 })
 export class ListDatesComponent implements OnInit {
+  user = new User();
+  dateList: DatesItem[];
 
-  constructor() { }
+  constructor(
+    private readonly userService: UserService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.loadUser();
   }
-
+  loadUser(): void {
+    this.userService.getUser()
+      .subscribe(user => {
+        this.dateList = user.dates;
+      });
+  }
 }
