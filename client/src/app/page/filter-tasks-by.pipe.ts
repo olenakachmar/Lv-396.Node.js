fdc51fcb273fb82e040055f04eab80540f4acbb4
GrishaShats <grishashats0@gmail.com>
2019-04-23 22:03:23 +0300

    Fix for pull

--- client/src/app/page/filter-tasks-by.pipe.ts
+++ client/src/app/page/filter-tasks-by.pipe.ts
@@ -1,58 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Filter } from './common/filter';
 import { Task } from './common/task';
-import { DateService } from './common/date.service';
+import { FilterByPipeService } from './common/filter-by-pipe.service';
 
 @Pipe({
   name: 'filterTasksBy'
 })
 export class FilterTasksByPipe implements PipeTransform {
   constructor(
-    private readonly dateService: DateService) {}
+    private readonly filterByPipeService: FilterByPipeService
+  ) {}
 
   transform(tasks: Task[], filters?: Filter[]): Task[] {
-    if (!filters || this.isAllFiltersTurnedOff(filters)) {
+    if (!filters || !tasks) {
       return tasks;
     }
-    if (!filters || this.isAllFiltersTurnedOff(filters)) {
+
+    if (this.filterByPipeServsice.isAllFiltersTurnedOff(filters)) {
       return tasks.filter(task => !(task.resolvedByAuthor && task.resolvedByPerformer));
     }
 
-    return tasks.filter(task => this.isTaskMatchesFilters(task, filters));
+    return tasks.filter(task => this.filterByPipeService.isTaskMatchesFilters(task, filters));
   }
-
-  private readonly isAllFiltersTurnedOff = (filters: Filter[]) =>
-    filters.every(filter => filter.defaultValue === -1);
-
-  private readonly isTaskMatchesFilters = (task: Task, filters: Filter[]): boolean =>
-    filters.every(filter => {
-      const meta = filter.name;
-      if (filter.defaultValue === -1) {
-        if (meta === 'type') {
-          return !(task.resolvedByAuthor && task.resolvedByPerformer);
-        }
-
-        return true;
-      }
-      const meta: string = filter.name;
-      if (meta === 'date') {
-        const date = (this.dateService.isDateString(task[meta])) ?
-          this.dateService.convertStringToDate(task[meta]) :
-          new Date(task[meta]);
-
-        return this.dateService.convertStringToDate(filter.defaultValue)
-          .getTime() === date.getTime();
-      }
-
-      if (meta === 'type' && filter.defaultValue === 2) {
-        return task.resolvedByAuthor && task.resolvedByPerformer;
-      }
-
-      if (meta === 'type' && filter.defaultValue === 2) {
-        return task.resolvedByAuthor && task.resolvedByPerformer;
-      }
-
-      return filter.defaultValue === task[meta].value;
-    });
-
 }
