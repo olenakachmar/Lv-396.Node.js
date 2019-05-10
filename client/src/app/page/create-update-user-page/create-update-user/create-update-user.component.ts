af5a3d340f5fbb8d59c64b0a7e6d0956a203cc48
GrishaShats <grishashats0@gmail.com>
2019-05-10 23:51:02 +0300

    Added functional for adding upcoming dates, fixed route for get issues

--- client/src/app/page/create-update-user-page/create-update-user/create-update-user.component.ts
+++ client/src/app/page/create-update-user-page/create-update-user/create-update-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { OptionPair } from '../../../common/models/option-pair';
+import { DatesItem } from '../../common/dates-item';
 
 @Component({
   selector: 'app-create-update-user',
@@ -9,23 +10,33 @@ import { OptionPair } from '../../../common/models/option-pair';
 })
 export class CreateUpdateUserComponent implements OnInit {
   profileForm: FormGroup;
+  datesForm: FormGroup;
   pairList: OptionPair[];
   contacts: string[];
+  dates: string[];
 
 
-  constructor(private readonly fb: FormBuilder) {
+  constructor(private readonly fb: FormBuilder,
+              private readonly date: FormBuilder) {
     this.profileForm = fb.group({
       email: ['', Validators.required],
       phone: ['', Validators.required],
       skype: ['', Validators.required],
       telegram: ['', Validators.required],
+      date: ['', Validators.required],
       contacts: this.fb.group({
+      })
+    });
+    this.datesForm = date.group({
+      date: ['', Validators.required],
+      dates: this.date.group({
       })
     });
   }
 
   ngOnInit() {
     this.contacts = [];
+    this.dates = [];
     this.pairList = [{_id: 1, name: 'telegram'}, {_id: 2, name: 'skype'}];
   }
 
@@ -33,6 +44,7 @@ export class CreateUpdateUserComponent implements OnInit {
     const item = `contact-${this.contacts.length}`;
     this.getContacts.addControl(item, new FormControl('', [Validators.required]));
     this.contacts = [...this.contacts, item];
+    console.log(this.contacts);
   }
 
   removeContact(control: string) {
@@ -42,5 +54,21 @@ export class CreateUpdateUserComponent implements OnInit {
 
   get getContacts() {
     return this.profileForm.get('contacts') as FormGroup;
+  }
+
+  addDate() {
+    const item = `date-${this.dates.length}`;
+    this.getDate.addControl(item, new FormControl('', [Validators.required]));
+    this.dates = [...this.dates, item];
+    console.log(this.dates);
+  }
+
+  removeDate(control: string) {
+    this.getDate.removeControl(control) ;
+    this.dates.pop();
+  }
+
+  get getDate() {
+    return this.datesForm.get('dates') as FormGroup;
   }
 }
